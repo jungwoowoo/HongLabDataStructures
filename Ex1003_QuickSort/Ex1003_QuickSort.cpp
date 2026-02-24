4353fbab6b7d87ffd6dab456b36e4eb28ac89763
JungWoo <woojungwoo34@medirita.com>
2026-02-24 16:02:00 +0900

    Update Ex1003_QuickSort/Ex1003_QuickSort.cpp using hint do while

--- Ex1003_QuickSort/Ex1003_QuickSort.cpp
+++ Ex1003_QuickSort/Ex1003_QuickSort.cpp
@@ -32,60 +32,84 @@ int Partition(int arr[], int low, int high, int n)
 	int i = low - 1;
 	int j = high + 1;
 
-	// int middle_index = size_t(floorf((high - low) / 2.0f)) + low;
-
-	// cout << "pivot=" << pivot << ", i=" << i << ", j=" << j << endl;
 	// cout << "middle_index=" << middle_index << endl;
+	// cout << "arr[i]=" << arr[i] << endl;
+	// cout << "arr[j]=" << arr[j] << endl;
+	// cout << "         ";
+	//Print(arr, low, high, n);
 
 	while (true)
 	{
 		// TODO:
-
-		// int middle_index = size_t(floorf((high - low) / 2.0f)) + low;
-
-		cout << "pivot=" << pivot << ", i=" << i << ", j=" << j << endl;
-		// cout << "middle_index=" << middle_index << endl;
-		// cout << "arr[i]=" << arr[i] << endl;
-		// cout << "arr[j]=" << arr[j] << endl;
+		//cout << "pivot=" << pivot << ", i=" << i << ", j=" << j << endl;
 		// cout << "         ";
-		Print(arr, low, high, n);
+		//Print(arr, low, high, n);
 
-		if( arr[i+1]<pivot && arr[j-1]>pivot )
+		do 
 		{
 			i=i+1;
-			j=j-1;
-		}
-		else if( arr[i+1]<pivot && arr[j-1]<=pivot )
-		{
-			i=i+1;
-		}
-		else if( arr[i+1]>=pivot && arr[j-1]>pivot )
+			//j=j-1;
+		}while( arr[i]<pivot );
+		//}while( arr[i]<pivot && arr[j]>pivot );
+
+		do 
 		{
 			j=j-1;
-		}		
-		else if( arr[i+1]>pivot && arr[j-1]<pivot )
-		{			
-			swap(arr[i+1],arr[j-1]);
-			i=i+1;
-			j=j-1;
+			//j=j-1;
+		}while( arr[j]>pivot );
+
+		
+
+		if( arr[i] > pivot && arr[j] < pivot ) swap(arr[i],arr[j]);
+
+		int pivot_index = size_t(floorf((high - low) / 2.0f)) + low;
+
+		if( i == pivot_index && j == pivot_index+1 ) 
+		{
+			cout << "after pivot=" << pivot << ", i=" << i << ", j=" << j << endl;
+			cout << "pivot_index=" << pivot_index << endl;
+
+			if( arr[i] > arr[j] ) swap(arr[i],arr[j]);
+
+			Print(arr, low, high, n);
+
+			return i;
 		}
+
+		// if( arr[i+1]<pivot && arr[j-1]>pivot )
+		// {
+		// 	i=i+1;
+		// 	j=j-1;
+		// }
+		// else if( arr[i+1]<pivot && arr[j-1]<=pivot )
+		// {
+		// 	i=i+1;
+		// }
+		// else if( arr[i+1]>=pivot && arr[j-1]>pivot )
+		// {
+		// 	j=j-1;
+		// }		
+		// else if( arr[i+1]>pivot && arr[j-1]<pivot )
+		// {			
+		// 	swap(arr[i+1],arr[j-1]);
+		// 	i=i+1;
+		// 	j=j-1;
+		// }
 	
-		else if( arr[i+1]==pivot && arr[j-1]<pivot )
-		{
-			swap(arr[i+1] , arr[j-1]);
-			return i+1;
-
-		}
-		else if( arr[i+1]>pivot && arr[j-1]==pivot )
-		{
-			swap(arr[i+1] , arr[j-1]);
-			return j-1;
-
-		}		
-		else if( arr[i+1]==pivot && arr[j-1]==pivot )
-		{
-			return i+1;
-		}
+		// else if( arr[i+1]==pivot && arr[j-1]<pivot )
+		// {
+		// 	swap(arr[i+1] , arr[j-1]);
+		// 	return i+1;
+		// }
+		// else if( arr[i+1]>pivot && arr[j-1]==pivot )
+		// {
+		// 	swap(arr[i+1] , arr[j-1]);
+		// 	return j-1;
+		// }		
+		// else if( arr[i+1]==pivot && arr[j-1]==pivot )
+		// {
+		// 	return i+1;
+		// }
 
 	}
 }
